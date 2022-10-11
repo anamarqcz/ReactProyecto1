3218aa9d6150c67aede341b09a36aa75c17128d4
Ana Marquez <ana.marquezc20@gmail.com>
2022-10-10 23:18:47 -0500

    se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//nucleo del proyecto
+//une los componentes
+import Header from "./componentes/Header"
+import ListadoPaciente from "./componentes/ListadoPaciente"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <ListadoPaciente/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/ListadoPaciente.jsx
@@ -0,0 +1,11 @@
+function ListadoPaciente (){
+  
+
+    return (
+      <h2>Listado Paciente</h2>
+      )
+  }
+  
+  export default ListadoPaciente
+
+ 
